03c2fc5aae7c0f4d99c487317923ae8841c034ca
Melvin Whitaker <awsome7mailman@gmail.com>
2024-05-09 17:46:15 -0600

    Fixed the Server Main Function.

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import chess.*;
+import dataaccess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        new Server(new SQLUserDAO(), new SQLAuthDAO(), new SQLGameDAO()).run(8080);
     }
 }
